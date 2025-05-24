d32d7ef7d85b028b6f7e64f83fc83c8ab203e948
Leonardo-Zappani <leonardozappani@alunos.utfpr.edu.br>
2025-05-24 00:22:37 -0300

    improving prediction controller

--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -1,59 +1,28 @@
 class PredictionsController < ApplicationController
-  IMAGE_DOG_PREDICT = "/assets/dog1.jpg"
-  IMAGE_CAT_PREDICT = "/assets/cat2.jpg"
-  IMAGE_DOG_LEARNING = "/assets/dog2.jpg"
-  IMAGE_CAT_LEARNING = "/assets/cat1.jpg"
-
-  def index
-    @prediction = Prediction.all
-  end
-
-  def show
-    @prediction = Prediction.find(params[:id])
-    if(@prediction.prediction == 1)
-      @image = IMAGE_DOG_PREDICT
-    else
-      @image = IMAGE_CAT_PREDICT
-    end
-  end
-
   def new
     @prediction = Prediction.new
   end
 
   def create
-    @prediction = Prediction.new(prediction_param)
-    @prediction.prediction = Prediction.new.predict(params[:height], params[:weight])
+    @prediction = Prediction.new(prediction_params)
+    @prediction.dm_label = Prediction.predict_dm!(
+      gender: params[:prediction][:gender],
+      age: params[:prediction][:age],
+      weight: params[:prediction][:weight],
+      height: params[:prediction][:height]
+    )
 
     if @prediction.save
-      redirect_to @prediction
+      render :show
     else
-      render 'new'
+      render :new
     end
   end
 
-  def update
-    if !Prediction.new.is_right_prediction(params[:id], params[:is_dog_person])
-      @prediction = Prediction.find(params[:id])
-      if(@prediction.is_dog_person == 1)
-        @image = IMAGE_CAT_LEARNING
-      else
-        @image = IMAGE_DOG_LEARNING
-      end
-      render 'learning'
-    end
-  end
-
-  def learning
-  end
-
   private
-  def prediction_param
-    params.require(:@prediction).permit(:height, :weight)
-  end
 
-  private
-  def update_prediction_param
-    params.permit(:is_dog_person)
+  def prediction_params
+    params.require(:prediction)
+          .permit(:gender, :age, :weight, :height)
   end
 end
